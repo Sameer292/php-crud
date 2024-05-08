f6eb694fda2400100d184483f634579acb0ffdac
ashishshres <ashishshr27@gmail.com>
2024-05-08 22:16:23 +0545

    refactor: change window location

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let addStudentBtn = document.querySelector(".add-student");
 
 addStudentBtn.addEventListener("click", () => {
-    window.location.href = "../php crud/create.php";
+    window.location.href = "./includes/create.php";
 });
