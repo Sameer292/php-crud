7c1a1570c5e2c609922bf6e20a9a1d6937484c4f
Strange Sam <i.am.strange@outlook.com>
2025-04-09 01:34:05 +0000

    Removed login/signin features

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-session_start();
-
-// check if the user is loggedin
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
-    header("location: ./includes/_login.php");
-    exit;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,19 +16,28 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
                 <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">🐘PHP CRUD</span>
                 </a>
-                <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse gap-4 items-center">
-                    <a href="./includes/_signup.php" class="font-medium text-blue-500 dark:text-yellow-500 hover:underline">Add Admin</a>
-                    <button type="button" class="logout text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="window.location.href='./includes/_logout.php'">Logout</button>
-                </div>
+               
             </div>
         </nav>
     </div>
     <div class="m-4">
-        <h1 class="text-3xl mb-10">Welcome <?= $_SESSION["username"] ?>👋</h1>
+        <h1 class="text-3xl mb-10">Welcome👋</h1>
         <button class="add-student text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Student</button>
     </div>
     <div class="relative overflow-x-auto shadow-md mt-5">
-        <table class="w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <?php
+                include "./includes/connect.php";
+                $id = 1;
+                // sql query to display from table
+                $sql = "SELECT * FROM students";
+                $result = mysqli_query($conn, $sql);
+
+                if (!$result) {
+                    die(mysqli_connect_error());
+                }
+
+                if (mysqli_num_rows($result) > 0) {
+                    echo '<table class="w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
@@ -61,19 +60,8 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
                     </th>
                 </tr>
             </thead>
-            <tbody>
-                <?php
-                include "./includes/connect.php";
-                $id = 1;
-                // sql query to display from table
-                $sql = "SELECT * FROM students";
-                $result = mysqli_query($conn, $sql);
-
-                if (!$result) {
-                    die(mysqli_connect_error());
-                }
-
-                if (mysqli_num_rows($result) > 0) {
+            <tbody>';
+                
                     // fetch each row from the table
                     while ($row = mysqli_fetch_assoc($result)) {
                         echo '
@@ -100,6 +88,8 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
                     </td>
                 </tr>';
                     }
+                }else{
+                    echo '<h1 class=" text-center text-bold text-2xl" >No Students added</h1>';
                 }
                 ?>
             </tbody>
